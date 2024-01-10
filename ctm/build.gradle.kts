c4d221000a631e2a96d520b1847861f451af72b2
Danylo Perepeluk <perepelukdanilo@gmail.com>
2024-01-10 12:51:59 +0200

    Update v

--- ctm/build.gradle.kts
+++ ctm/build.gradle.kts
@@ -66,7 +66,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.orioneee"
                 artifactId = "ctm"
-                version = "1.0.1"
+                version = "1.0.2"
                 from(components["release"])
             }
         }
